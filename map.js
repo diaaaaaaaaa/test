0d9258662a514466e0f6c5c078bf65227874a2d4
diaaaaaaaaa <95875767+diaaaaaaaaa@users.noreply.github.com>
2024-12-22 23:04:46 +0800

    Add files via upload

---
+++ map.js
@@ -0,0 +1,26 @@
+let hero = document.querySelector(".hero");
+let slider = document.querySelector(".slider");
+let animation = document.querySelector("section.animation-wrapper");
+
+const time_line = new TimelineMax();
+
+time_line
+    .fromTo(hero, 1, {height: "0%"}, {height: "100%" , ease: Power2.easeInOut }
+        
+    
+).fromTo(hero, 1.2, { width: "80%"}, {width: "100%" , ease: Power2.easeInOut }
+        
+    
+).fromTo(
+    slider, 
+    1, 
+    {x:"-100%"}, 
+    {x: "0%",ease: Power2.easeInOut }, 
+    "-=1.2"
+)
+.fromTo(animation, 0.3, {opacity: 1},{opacity: 0} );
+
+setTimeout(() => {
+    animation,Style.pointerEvents="none";
+
+}, 2500);
